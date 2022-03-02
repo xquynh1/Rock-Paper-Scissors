be2df3968274cdaa4cede96654f7c0d674c20f4c
Nguyen Thao Xuan Quynh <xquynh0812@gmail.com>
2022-03-02 20:43:06 +0700

    Finish playRound() function

--- game.js
+++ game.js
@@ -1,7 +1,49 @@
 function computerPlay() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * choices.length);
-    console.log(choices[random]);
+    console.log('Computer chooses ' + (choices[random]));
+    return choices[random];
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === "rock") {
+        if (playerSelection.toLowerCase() === "rock") {
+            console.log(`Computer also chose ${computerSelection}! It\'s a draw`);
+            return `Computer also chose ${computerSelection}! It\'s a draw`;
+        } else if (playerSelection.toLowerCase() === "scissors") {
+            console.log(`You lose! Computer chose ${computerSelection}`);
+            return `You lose! Computer chose ${computerSelection}`;
+        } else 
+        console.log(`You win! Computer chose ${computerSelection}`);
+        return `You win! Computer chose ${computerSelection}`;
+    }
+
+    if (computerSelection === "paper") {
+        if (playerSelection.toLowerCase() === "paper") {
+            console.log(`Computer also chose ${computerSelection}! It\'s a draw`);
+            return `Computer also chose ${computerSelection}! It\'s a draw`;
+        } else if (playerSelection.toLowerCase() === "rock") {
+            console.log(`You lose! Computer chose ${computerSelection}`);
+            return `You lose! Computer chose ${computerSelection}`;
+        } else 
+        console.log(`You win! Computer chose ${computerSelection}`);
+        return `You win! Computer chose ${computerSelection}`;
+    }
+
+    if (computerSelection === "scissors") {
+        if (playerSelection.toLowerCase() === "scissors") {
+            console.log(`Computer also chose ${computerSelection}! It\'s a draw`);
+            return `Computer also chose ${computerSelection}! It\'s a draw`;
+        } else if (playerSelection.toLowerCase() === "rock") {
+            console.log(`You lose! Computer chose ${computerSelection}`);
+            return `You lose! Computer chose ${computerSelection}`;
+        } else 
+        console.log(`You win! Computer chose ${computerSelection}`);
+        return `You win! Computer chose ${computerSelection}`;
+    }
+}
+
+playRound("ROcK",computerPlay());
+
+
+
